203f1cf2ca3d4d2c044219ab105dbb3a9fb58824
sheshadriN <sheshadri.n2021ecec@sece.ac.in>
2024-05-14 18:38:54 +0530

    written a simple server

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello")
+	new()
+	var handeler = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello %v", r.URL.Path)
+	}
+	http.HandleFunc("POST /hello", handeler)
+	var error = http.ListenAndServe(":9000", nil)
+	fmt.Println("sever is running at port 9000")
+	if error!=nil{
+		log.Fatal(error)
+	}
+}
+func new(){
+	var handeler = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello %v", r.URL.Path)
+	}
+	http.HandleFunc("POST /hello", handeler)
+	var error = http.ListenAndServe(":9001", nil)
+	fmt.Println("sever is running at port 9000 second")
+	if error!=nil{
+		log.Fatal(error)
+	}
 }
