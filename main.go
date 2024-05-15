5d17f992269da156d6609e328ec78752e55b59c6
sheshadriN <sheshadri.n2021ecec@sece.ac.in>
2024-05-15 19:02:20 +0530

    arrays learned and garnge value

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	array()
 	new()
 	var handeler = func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello %v", r.URL.Path)
@@ -27,5 +28,14 @@ func new(){
 	fmt.Println("sever is running at port 9000 second")
 	if error!=nil{
 		log.Fatal(error)
+	}
+}
+
+
+func array(){
+	// var arr = [10]string{"1","2","3","4"}
+	arr2 := [10]int{1,2,3,4,5,6,7,8,9,0}
+	for i:=0;i<10;i++{
+		fmt.Print((arr2[i]))
 	}
 }
